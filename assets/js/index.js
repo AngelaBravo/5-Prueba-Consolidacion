1111ff8fc5cda72a5919a6f4d6dcad0a76d6a027
Karytha <angelabravop@gmail.com>
2024-09-02 20:58:22 -0400

    completado con fetch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,50 @@
+const urls = Array.from({ length: 15 }, (_, i) => `https://swapi.dev/api/people/${i + 1}/`);
+const personajes = [];                 // un arreglo donde se podria almacenar toda la data necesaria (los 15.)
+const inicios = {
+    vip:0,
+    sec:5,
+    otros:10
+}
+
+const solicitarPersonajes = async () => {
+  try {
+    const respuesta = await Promise.all(urls.map(url => fetch(url)));
+    const data = await Promise.all(respuesta.map(response => response.json()));
+    
+    const filteredData = data.map(person => ({ // crea un arreglo con los 3 datos obtenidos de la url swapi
+      nombre: person.name,
+      altura: person.height + ' cms.',
+      peso: person.mass + ' kgs.'
+    }));
+    
+    personajes.push(...filteredData);     // arreglo con la misma data para otros usos.
+    
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+solicitarPersonajes();
+console.log(personajes);
+
+
 document.body.addEventListener('mouseover', mostrarCarta);
 
 function mostrarCarta(e) {
   const t = e.target;
   if (t.classList.contains('uno')) {
-    const category = e.target.dataset.categoria
+    const category = e.target.dataset.categoria          // <p class="uno" data-categoria="vip">1 - 5</p>
     const tarjeta = document.getElementById(category)
     const howMany = tarjeta.children.length;
     if (howMany < 5) {
-      
-      const urls = Array.from({ length: 5 }, (_, i) => `https://swapi.dev/api/people/${i + 1}/`);
-      const renderedData = [];                  // un arreglo donde se podria almacenar toda la data necesaria.
-      const fetchAllData = async () => {
-        try {
-          const responses = await Promise.all(urls.map(url => fetch(url)));
-          const data = await Promise.all(responses.map(response => response.json()));
-          
-          const filteredData = data.map(person => ({
-            name: person.name,
-            height: person.height + ' cms.',
-            mass: person.mass + ' kgs.'
-          }));
-          
-          renderedData.push(...filteredData);     // arreglo con la misma data para otros usos.
-          renderData(filteredData);
-          
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-      
-      fetchAllData();
-      console.log(renderedData);
-      console.log(e.name);
-      
-      function renderData(data) {
-        data.forEach((e, i) => {
-          const p = document.createElement('p');
-          p.textContent = `${i+1}. ${e.name} - Estatura: ${e.height} Peso: ${e.mass}`;
-          document.body.appendChild(p)
-        })
-      }
+    
 
       let cardUno = `
         <div class=" uno single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
           <div class="timeline-icon" style="background-color: rgb${category==="vip"? "(190, 13, 13)":category==="sec"? "(48, 185, 48)":"(0, 255, 255)"};"></div>
           <div class="timeline-text">
-            <h6>${e.name}</h6>
-            <p>Estatura: ${e.height} Peso: ${e.mass}</p>
+            <h6>${personajes[inicios[category] + howMany].nombre}</h6>
+            <p>Estatura: ${personajes[inicios[category]+howMany].altura} Peso: ${personajes[inicios[category]+howMany].peso}</p>
           </div>
         </div>
       `;
